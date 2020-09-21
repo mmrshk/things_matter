c83e866a686c11014ff9acd95a2ff31545fd3ed6
Emma <emma.yeroshek@gmail.com>
2020-09-21 20:58:49 +0300

    Fix locales

--- spec/requests/graphql/mutations/move/project/in_area_spec.rb
+++ spec/requests/graphql/mutations/move/project/in_area_spec.rb
@@ -52,7 +52,7 @@ describe 'mutation moveProjectInTask', type: :request do
 
     context 'when raise ActiveRecord::ActiveRecordError' do
       # rubocop:disable RSpec/AnyInstance
-      before { allow_any_instance_of(TaskProject).to receive(:update!).and_raise(ActiveRecord::ActiveRecordError) }
+      before { allow_any_instance_of(TaskgProject).to receive(:update!).and_raise(ActiveRecord::ActiveRecordError) }
       # rubocop:enable RSpec/AnyInstance
 
       it 'returns execution error data' do
