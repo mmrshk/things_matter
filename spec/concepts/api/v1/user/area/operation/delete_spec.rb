0b59cc1a7b73f7f6917f8eab0a9d7b71cd83086c
Emma <emma.yeroshek@gmail.com>
2020-10-19 20:05:08 +0300

    Fix delete area

--- spec/concepts/api/v1/user/area/operation/delete_spec.rb
+++ spec/concepts/api/v1/user/area/operation/delete_spec.rb
@@ -4,14 +4,19 @@ describe Api::V1::User::Area::Operation::Delete, type: :operation do
   subject(:execute_operation) { described_class.call(params: params, current_user: current_user) }
 
   let(:current_user) { create(:user_account) }
-  let(:area) { create(:task_area, user_account: current_user) }
   let(:name) { FFaker::Lorem.word }
 
   let(:params) { { id: area.id } }
 
-  before { area }
-
   context 'when user deletes area' do
+    let!(:area) { create(:task_area, user_account: current_user, with_projects: true) }
+
+    it 'changes relationships' do
+      execute_operation
+
+      expect(area.task_projects.pluck(:task_area_id).uniq.first).to eq(nil)
+    end
+
     it 'deletes area' do
       expect { execute_operation }.to change(TaskArea, :count).from(1).to(0)
     end
@@ -26,6 +31,7 @@ describe Api::V1::User::Area::Operation::Delete, type: :operation do
   end
 
   context 'when area id NOT exist' do
+    let!(:area) { create(:task_area, user_account: current_user, with_projects: true) }
     let(:params) { { id: SecureRandom.uuid } }
 
     it 'NOT deletes area' do
@@ -38,7 +44,7 @@ describe Api::V1::User::Area::Operation::Delete, type: :operation do
   end
 
   context 'when user not authorized to action' do
-    let(:area) { create(:task_area) }
+    let!(:area) { create(:task_area) }
 
     it 'NOT deletes area' do
       expect { execute_operation }.not_to change(TaskArea, :count)
