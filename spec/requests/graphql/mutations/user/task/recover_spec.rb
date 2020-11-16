80cb13042acd1f5d07bb569bf4c46c585010eebe
Emma <emma.yeroshek@gmail.com>
2020-11-16 16:52:26 +0200

    Add specs

--- spec/requests/graphql/mutations/user/task/recover_spec.rb
+++ spec/requests/graphql/mutations/user/task/recover_spec.rb
@@ -4,52 +4,48 @@ describe 'mutation userTaskRecover', type: :request do
   let(:user_account) { create(:user_account) }
   let(:token) { generate_token(account_id: user_account.id) }
 
-  let(:variables) { { input: { id: note.id } } }
+  let(:variables) { { input: { id: task.id } } }
 
-  let(:note) { create(:note, note_project: project) }
-  let(:project) { create(:note_project, user_account: user_account) }
+  let(:task) { create(:task, user_account: user_account) }
 
   context 'when success' do
     context 'when exist project' do
-      it 'returns updated note of current_user' do
+      it 'returns updated task of current_user' do
         authorized_graphql_post(
-          query: user_note_recover_mutation,
+          query: user_task_recover_mutation,
           variables: variables,
           auth_token: token
         )
 
-        expect(response).to match_schema(UserNote::RecoverSchema)
+        expect(response).to match_schema(UserTask::RecoverSchema)
         expect(response.status).to be(200)
       end
     end
 
     context 'when project NOT exist' do
-      let(:project) { create(:note_project, user_account: user_account) }
-      let(:note) { create(:note, user_account: user_account) }
-      let(:variables) { { input: { id: note.id, note_project_id: project.id } } }
+      let(:task) { create(:task, user_account: user_account, without_task_project: true) }
+      let(:variables) { { input: { id: task.id, task_location: 'TODAY' } } }
 
-      before { note.note_project.destroy }
-
-      it 'returns updated note of current_user' do
+      it 'returns updated task of current_user' do
         authorized_graphql_post(
-          query: user_note_recover_mutation,
+          query: user_task_recover_mutation,
           variables: variables,
           auth_token: token
         )
 
-        expect(response).to match_schema(UserNote::RecoverSchema)
+        expect(response).to match_schema(UserTask::RecoverSchema)
         expect(response.status).to be(200)
       end
     end
   end
 
   context 'when failed' do
-    context 'when note not found' do
+    context 'when task not found' do
       let(:variables) { { input: { id: SecureRandom.uuid } } }
 
       it 'returns execution error data' do
         authorized_graphql_post(
-          query: user_note_recover_mutation,
+          query: user_task_recover_mutation,
           variables: variables,
           auth_token: token
         )
@@ -62,7 +58,7 @@ describe 'mutation userTaskRecover', type: :request do
     context 'when user not authorized' do
       it 'returns error' do
         graphql_post(
-          query: user_note_recover_mutation,
+          query: user_task_recover_mutation,
           variables: variables
         )
 
